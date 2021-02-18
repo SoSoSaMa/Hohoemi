4a811a7802c347698c0f712e9bc600c1eb918a40
SoSoSaMa <leibingzhe2@gmail.com>
2021-02-18 23:03:42 +0800

    First Project 2

--- data/stock.h
+++ data/stock.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <iostream>
 #include <sstream>
 
 class Stock {
--- main.cpp
+++ main.cpp
@@ -2,11 +2,10 @@
 // Created by leibi on 2021/2/18.
 //
 
-#include "Hohoemi.h"
 #include "data/stock.h"
 
 int main() {
-    Stock instance;
+    Stock instance(10.0, 20.0, 5.0, 25.0, 1613659249);
 
     std::cout << instance.description()
               << std::endl;
